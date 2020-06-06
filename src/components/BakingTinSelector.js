6ce16218523840a05740ebb5d46e0bd32d47309e
Richard Harris <rjh@netcraft.com>
2020-06-06 18:50:24 +0100

    move NumInput to its own component and add useCallback
    This solves the issue of losing focus as the entire tin selector
    is re-rendered on update.

--- src/components/BakingTinSelector.js
+++ src/components/BakingTinSelector.js
@@ -4,6 +4,24 @@ const shapes = ["Round", "Square"];
 const shapeGlyphs = ["⚪", "□"];
 const units = ["cm", "inches"];
 
+const NumInput = ({ label, field, value, onChange }) => {
+  return (
+    <label>
+      {label}
+      {": "}
+      <input
+        type="number"
+        required
+        step="any"
+        min="0.0001"
+        name={field}
+        value={value}
+        onChange={onChange}
+      />
+    </label>
+  );
+};
+
 export default function BakingTinSelector({ tin: inputTin, onUpdate }) {
   let tin = { ...inputTin };
 
@@ -13,28 +31,10 @@ export default function BakingTinSelector({ tin: inputTin, onUpdate }) {
     tin.unit = units[0];
   }
 
-  const handleUpdate = (e) => {
+  const handleUpdate = React.useCallback((e) => {
     tin[e.target.name] = e.target.value;
     onUpdate(tin);
-  };
-
-  const NumInput = ({ label, field }) => {
-    return (
-      <label>
-        {label}
-        {": "}
-        <input
-          type="number"
-          required
-          step="any"
-          min="0.0001"
-          name={field}
-          value={tin[field]}
-          onChange={handleUpdate}
-        />
-      </label>
-    );
-  };
+  }, [onUpdate]);
 
   return (
     <form className="tin-selector">
@@ -54,13 +54,13 @@ export default function BakingTinSelector({ tin: inputTin, onUpdate }) {
       <br />
       {tin.shape === "Square" ? (
         <>
-          <NumInput label="Width" field="width" />
-          <NumInput label="length" field="length" />
+          <NumInput label="Width" field="width" value={tin.width} onChange={handleUpdate} />
+          <NumInput label="length" field="length" value={tin.length} onChange={handleUpdate} />
         </>
       ) : (
-        <NumInput label="Diameter" field="diameter" />
+        <NumInput label="Diameter" field="diameter"  value={tin.diameter} onChange={handleUpdate} />
       )}
-      <NumInput label="Height" field="height" />
+      <NumInput label="Height" field="height" value={tin.height} onChange={handleUpdate} />
       <select name="unit" value={tin.unit} onChange={handleUpdate}>
         <option value="cm">cm</option>
         <option value="in">inches</option>
